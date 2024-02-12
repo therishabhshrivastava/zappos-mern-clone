1421805c738279def49680365c49ebd871607d7d
therishabhshrivastava <you@example.com>
2024-02-12 23:33:41 +0530

    footer

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,18 @@
+
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+import Banner from "../components/Banner";
+import Popular from "../components/Popular";
+
+function Landing() {
+    
+    return(
+        <>
+        <Navigation />
+        <Banner />
+        <Popular />
+        <Footer />
+        </>
+    )
+}
+export default Landing
